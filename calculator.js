35312a656a7cde8625e97d012d5698e9b5a77053
Brandon Culp <bculp513@gmail.com>
2017-01-26 22:58:27 -0500

    mobile is working againbut need to setup shadow effect for key events

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
-const output = document.querySelector('#output');
+// mobile stopped working when changed event listeners so had to make output var instead of const
+var output = document.querySelector('#output');
 const buttons = document.querySelectorAll('.item');
 let equation = '';
 let validNums = ['0','1','2','3','4','5','6','7','8','9','.'];
@@ -7,11 +8,7 @@ let result = 0;
 
 function pressButton(...args) {
 
-	//this will take off the box shadow when buton is pressed. seems like what i want but
-	// might have to change to divs to see for sure
-	// console.log(this)
-	this.classList.add('button-pressed')
-
+	this.classList ? this.classList.add('button-pressed') : '';
 
 	let input = args[1] || this.textContent;
 	console.log(input)
@@ -34,6 +31,9 @@ function pressButton(...args) {
 
 function compute(equation) {
 	// should shop off last element if it's an operator or decimal
+	// handle the case that input or output is longer than the allowed space in the text box
+	// round for output but stop adding for input
+	// button pressing effect doesn't work on mobile. Class is added, but don't see effect.
 
 	// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
 	equation = equation.replace(/\u00D7/g, '*').replace(/\u00F7/g, '/');
@@ -44,16 +44,28 @@ function compute(equation) {
 buttons.forEach(button => {
 	button.addEventListener('mousedown', pressButton);
 	button.addEventListener('mouseup', function() {
-		console.log(this.classList)
 		if (this.classList.contains('button-pressed')) {
 			this.classList.remove('button-pressed');
 		}
 	});
 })
 
-document.addEventListener('keypress', (e) => {
+document.addEventListener('keydown', function(e) {
 	if (validNums.includes(e.key) || validOperators.includes(e.key) || e.key === 'Enter') {
+		console.log('e is: ', e.key)
+		console.log(buttons[4])
+		// buttons[4].classList.add('button-pressed');
+		// console.log('this is:', this)
 		pressButton(null, e.key)
-
 	}
+})
+
+document.addEventListener('keyup', function() {
+	// if (this.classList.contains('button-pressed')) {
+	// 	this.classList.remove('button-pressed');
+	// }
+		// buttons[4].classList.remove('button-pressed');
+			console.log('key event')
+
+
 })
