9514ded066cf10d2c2b52a4492efbb7d75f6566a
Brandon Culp <bculp513@gmail.com>
2017-02-05 18:15:41 -0500

    renamed calculator so browser sync works for timer and setup basic timer html and css

---
+++ timer.js
@@ -0,0 +1,5 @@
+const timerText = document.querySelector('#timer-text');
+const returnText = document.querySelector('#return-text');
+
+// buttons at top or side for set times and then a place to enter a time
+// or just click on the timer to set time
